69e72f48b5c5a58f683310ef5ab6a4a3cce04600
Paul Lee <7.paul.lee@gmail.com>
2016-09-24 18:36:42 -0600

    Requesting Tyler to look at

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,25 +6,40 @@ test = ["adriana", "paul"]
 # 3. declare a hash
 
 letters = ["a", "b", "c", "d"]
-
 letters_hash = {1=> 'one', 2 => 'two', 3 => 'three'}
 
-["a", "b", "c", "d"].each {|b| puts "hello alphabet #{b}"}
+p letters
+p letters_hash
+
+letters.each do |letters_repeat| 
+4.times {puts "hello alphabet #{letters_repeat}"}
+end
+
+letters_hash.each do |numerical, alpha| 
+3.times {puts "we have #{numerical}, respectively #{alpha}!"}
+end
+puts "After .each call"
 
 [letters].map {|letters| puts "hello alpha #{letters}"}
 [letters].map! {|letters| puts "hello alpha #{letters}"}
 
+p "-" * 20 + "release 2" + "-" * 20 
 # ============= RELEASE 2 ==========================
 arr = [1,2,3,4,5,6,7]
 arr_hash = {1=> 'one', 2 => 'two', 3 => 'three', 4=> 'four', 5 => 'five', 6 => 'six', 7 => 'seven'}
+p arr
+p arr_hash
 
 arr.delete_if { |a| a < 4 }
-arr_hash.delete(5) {|test| puts "#{test} not found"}
+arr_hash.delete_if {|test| test < 3}
 #q: tyler: how do we print this to check?
+puts arr
+puts arr_hash
+p "-" * 20 
 
-arr.select {|num| num.even? }
-arr_hash.select {|k,v| v > 3 }
-#q: tyler: why does this fail? hash fail?
+# arr.select {|num| num.even? }
+# arr_hash.select {|k,v| v > 3 }
+# #q: tyler: why does this fail? hash fail?
 
 arr.sort { |x,y| y<=> x }
 #q: tyler: why do we need both x and y
