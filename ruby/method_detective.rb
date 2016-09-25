7591de8045bb21638cd58abe9210ed8fbe215e77
Paul Lee <7.paul.lee@gmail.com>
2016-09-24 18:01:41 -0600

    Added puts

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase 
-=> “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase 
+#=> “InVeStIgAtIoN”
 
-"zom".insert(2, 'o')
-=> “zoom”
+puts "zom".insert(2, 'o')
+#=> “zoom”
 
-"enhance".center(20)
-=> "    enhance    "
+puts "enhance".center(20)
+#=> "    enhance    "
 
-"Stop! You’re under arrest!".capitalize
-=> "STOP! YOU’RE UNDER ARREST!"
+puts "Stop! You’re under arrest!".upcase
+#=> "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".insert(-1, 'suspects')
-=> "the usual suspects"
+puts "the usual".insert(-1, ' suspects')
+#=> "the usual suspects"
 
-" suspects".insert(0, 'the usual')
-=> "the usual suspects"
+puts " suspects".insert(0, 'the usual')
+#=> "the usual suspects"
 
-"The case of the disappearing last letter".chop
-=> "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chop
+#> "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete(1)
-=> "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".delete("T")
+#=> "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
-=> "Elementary, my dear Watson!"
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
+#=> "Elementary, my dear Watson!"
 
-"z".octo
-=> 122 
-(What is the significance of the number 122 in relation to the character z?)
+puts "z".octo
+#=> 122 
+#(What is the significance of the number 122 in relation to the character z?)
 
-"How many times does the letter 'a' appear in this string?".index('a')
+puts "How many times does the letter 'a' appear in this string?".index('a')
 => 4
