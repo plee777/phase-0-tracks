56f5ebf391c521c5570f3b167560564b5b36b1c2
Paul Lee <7.paul.lee@gmail.com>
2016-09-30 16:34:59 -0600

    Satisfied tests.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,25 +1,31 @@
 class TodoList
-
-	def get_items(todolist_array)
-		todolist_array
+	def initialize(todolist_array)
+		@todolist_array = todolist_array 
 	end
 
-	def add_item(new_item,todolistod_array)
-		todolist_array.push(new_item)
-		todolist_array
+	def get_items
+		@todolist_array
 	end
 
-	def delete_item(x)
-		p 'mow the lawn'
+	def add_item(new_item)
+		@todolist_array.push(new_item)
+	end
+
+	def delete_item(old_item)
+		@todolist_array.delete(old_item)
+
+	end
+
+	def get_item(old_item)
+	@todolist_array[old_item]
 
 	end
 
 end
 
-todolist_array = ["do the dishes", "mow the lawn"]
-
-todolist = TodoList.new
-p todolist.get_items(todolist_array)
-p todolist.add_item("do laundry",todolist_array)
-# p todolist.delete_item('do the dishes')
+todolist = TodoList.new(["do the dishes","mow the lawn"])
+p todolist.get_items
+p todolist.add_item("do laundry")
+p todolist.delete_item('do the dishes')
+p todolist.get_item(0)
 
