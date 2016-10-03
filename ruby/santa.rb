a2595a5e3b8917c284383569d2ec554690b57f2b
Paul Lee <7.paul.lee@gmail.com>
2016-10-03 06:06:30 -0600

    added methods and reader/accessor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,22 +1,38 @@
 class Santa
-	def initialize
+	attr_reader :ethnicity
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity)
 		p "initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
 	end
 
 	def speak
-		p "ho ho ho happy holidays"
+		p "#{@gender} #{@ethnicity} dude says - ho ho ho happy holidays"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
 		p "That was a good #{cookie_type}!"
 	end
 
+	def celebrate_birthday
+
+	end
+
+	def get_mad_at(name)
+
+	end
+
+
+# reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+# age = 0
 end
 
-santa = Santa.new
-
-santa.initialize
+santa = Santa.new("korean", "male")
 santa.speak
-santa.eat_milk_and_cookies(best_cookie)
+santa.eat_milk_and_cookies("best_cookie")
+santa.celebrate_birthday
+santa.get_mad_at("paul")
 
 
