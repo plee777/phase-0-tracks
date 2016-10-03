279c56df4be545b8ce3af6416bdc95aab341cd3e
Paul Lee <7.paul.lee@gmail.com>
2016-10-03 11:08:44 -0600

    Added driver codes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,4 +3,13 @@ module Shout
 		words + "!!!" + ":("
 	end
 
+		def yelling_happily(words)
+		words + "omg" + "so happy"
+	end
+
 end
+
+#driver codes.
+Shout.self.yell_angrily("BOOYAH")
+Shout.yelling_happily("hell yeah")
+
