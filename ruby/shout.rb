27c13054445d5e67edd06e7500fd500d9889ee22
Paul Lee <7.paul.lee@gmail.com>
2016-10-03 11:13:53 -0600

    Changed code to Mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,21 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + ":("
+# 	end
+
+# 		def yelling_happily(words)
+# 		words + "omg" + "so happy"
+# 	end
+
+# end
+
+# #driver codes.
+# Shout.self.yell_angrily("BOOYAH")
+# Shout.yelling_happily("hell yeah")
+
+
+########RELEASE 3 ###########
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + ":("
-	end
-
-		def yelling_happily(words)
-		words + "omg" + "so happy"
-	end
-
-end
-
-#driver codes.
-Shout.self.yell_angrily("BOOYAH")
-Shout.yelling_happily("hell yeah")
+	def yelling(words)
+		words + "!!!"
 
